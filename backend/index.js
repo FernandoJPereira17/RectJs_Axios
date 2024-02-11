46fa5165db1815e789c4f9f33a09bd2480f4f0be
FernandoJPereira <fernandojosepereira17@gmail.com>
2024-02-11 18:16:23 -0300

    ReactJs_Axios

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const server = express();
+const cors = require('cors');
+
+server.use(cors()) //Essa linha aqui
+
+
+const alunos = ['Wesley', 'Marina', 'Bruno', 'Paula'];
+
+server.get('/alunos', (req, res) =>{
+    return res.json(alunos)
+})
+
+server.listen(4000)
+
